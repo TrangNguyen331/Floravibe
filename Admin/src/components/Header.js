df7a5f7bc936adb8b6df9d18374319e45fa99132
Hanh Nguyen <nguyenkimhanh1532@gmail.com>
2023-12-21 23:09:15 +0700

    update header, css

--- Admin/src/components/Header.js
+++ Admin/src/components/Header.js
@@ -53,20 +53,8 @@ function Header() {
         >
           <MenuIcon className="w-6 h-6" aria-hidden="true" />
         </button>
-        {/* <!-- Search input --> */}
-        <div className="flex justify-center flex-1 lg:mr-32">
-          <div className="relative w-full max-w-xl mr-6 focus-within:text-purple-500">
-            <div className="absolute inset-y-0 flex items-center pl-2">
-              <SearchIcon className="w-4 h-4" aria-hidden="true" />
-            </div>
-            <Input
-              className="pl-8 text-gray-700"
-              placeholder="Search for projects"
-              aria-label="Search"
-            />
-          </div>
-        </div>
-        <ul className="flex items-center flex-shrink-0 space-x-6">
+
+        <ul className="flex items-center flex-shrink-0 space-x-6 ml-auto">
           {/* <!-- Profile menu --> */}
           <li className="relative">
             <button
